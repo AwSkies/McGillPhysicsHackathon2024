e8247be7205ab520780689cffd5dfac9e7501ff4
Jasmine Boyd <jasminevb5017@gmail.com>
2024-11-16 13:42:44 -0500

    Create and test initial render function

--- erosion/render.py
+++ erosion/render.py
@@ -1,18 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-from skimage import measure
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-#state - 3D array each entry- 0 air 1 water 2 rock
-#create mesh out of that create open USD file
-#filename- USD file name
-#
+from skimage import measure
+from skimage.draw import ellipsoid
 
-def render(state, filename):
+#state - 3D array each entry- 0 air 1 water 2 rock
+#create mesh out of that 
+
+def render(state):
     #do marching cubes do generate rendering?
-    vertsW, facesW, normalsW, valuesW = measure.marching_cubes(state, 1)
-    vertsR, facesR, normalsR, valuesR = measure.marching_cubes(state, 2)
+    vertsW, facesW, normalsW, valuesW = measure.marching_cubes(state, 0)
+    vertsR, facesR, normalsR, valuesR = measure.marching_cubes(state, 1)
 
     #plot the thingy
     fig = plt.figure(figsize=(10, 10))
@@ -20,4 +19,33 @@ def render(state, filename):
 
     #create mesh for rock
     meshR = Poly3DCollection(vertsR[facesR])
-    meshR.set_color("brown")
+    meshR.set_color("sienna")
+    meshR.set_edgecolor("k")
+    ax.add_collection3d(meshR)
+
+    #create mesh for water
+    meshW = Poly3DCollection(vertsW[facesW])
+    meshW.set_color("blue")
+    meshW.set_edgecolor("k")
+    ax.add_collection3d(meshW)
+
+    ax.set_xlim(0)
+    ax.set_ylim(0)
+    ax.set_zlim(0)
+
+    plt.tight_layout()
+    plt.show()
+
+arr = np.zeros((5, 5, 5))
+
+for x in range(5):
+    for y in range(5):
+        for z in range(5):
+            if z < 2:
+                arr[x, y, z] = 2
+            elif z < 3:
+                arr[x, y, z] = 1
+            else:
+                arr[x, y, z] = 0
+
+render(arr)
