d6acb75b5c6f942a48fb19da8a07d78be121d22b
Jasmine Boyd <jasminevb5017@gmail.com>
2024-11-16 11:51:51 -0500

    Add starting render code

--- erosion/__init__.py
+++ erosion/__init__.py
@@ -1,1 +1,2 @@
 from .fluid_sim import Erosion
+from .render import render
---
+++ erosion/render.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+#state - 3D array each entry- 0 air 1 water 2 rock
+#create mesh out of that create open USD file
+
+def render(state, filename):
+    pass
